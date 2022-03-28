d7edf371b02840c46206181c223ba7c1ec57635c
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-03-28 18:28:39 -0400

    My first Golang program

---
+++ hello.go
@@ -0,0 +1,11 @@
+// REST API Overview
+// Javascript/node.js : quick to run
+// Golang: scales with type error handling
+
+package main
+
+import "fmt"
+
+func main() {
+   fmt.Println("Hello World")
+}
